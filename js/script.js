532d46307cb14d9f8a1c7fe5c3c88cea984466de
lexoyo <a.hoyau@silexlabs.org>
2014-11-28 20:36:49 +0100

    with ab testing code

--- js/script.js
+++ js/script.js
@@ -64,13 +64,14 @@ else{
 
 ////////////////////////////////////
 // google analytics
-(function(i,s,o,g,r,a,m){i['Google◊◊yticsObject']=r;i[r]=i[r]||function(){
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-19608894-18', 'silex.me');
+ga('create', 'UA-19608894-18', 'auto');
 ga('send', 'pageview');
+
 
 // track links
 $(function(){
@@ -90,4 +91,3 @@ $(function(){
     }
 });
 ////////////////////////////////////
-
