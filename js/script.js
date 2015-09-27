57d7a106940b1822e16247030769ce21b5e0b593
lexoyo <a.hoyau@silexlabs.org>
2015-09-27 12:08:35 +0200

    comparison matrix from wikipedia

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@
                                 scrollTop: pos
                             }, 500);
                     }
-            
+
                     ////////////////////////////////////
                     // github issues
                     var widgetScriptUrl = 'js/widgets.js';
@@ -65,17 +65,17 @@
                       if (typeof console !== "undefined") console.error('NO WEBWORKER');
                       document.write('<script src="'+widgetScriptUrl+'"></'+'script>')
                     }
-            
+
                     ////////////////////////////////////
                     // google analytics
                     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            
+
                     ga('create', 'UA-19608894-23', 'auto');
                     ga('send', 'pageview');
-            
+
                     // track links
                     $(function(){
                         $('a, [data-silex-href]').click(function(){
