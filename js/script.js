575fc720381c0fd4268bef30996484d385f793b7
Alex Hoyau <a.hoyau@silexlabs.org>
2020-11-09 13:26:09 +0100

    update silex version

--- js/script.js
+++ js/script.js
@@ -101,17 +101,17 @@ $(function() {
             });
         }
   
-    $("#js-rotating").Morphext({
-        // The [in] animation type. Refer to Animate.css for a list of available animations.
-        animation: "fadeIn",
-        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-        separator: ",",
-        // The delay between the changing of each phrase in milliseconds.
-        speed: 3000,
-        complete: function () {
-            // Called after the entrance animation is executed.
-        }
-    });
+    // $("#js-rotating").Morphext({
+    //     // The [in] animation type. Refer to Animate.css for a list of available animations.
+    //     animation: "fadeIn",
+    //     // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    //     separator: ",",
+    //     // The delay between the changing of each phrase in milliseconds.
+    //     speed: 3000,
+    //     complete: function () {
+    //         // Called after the entrance animation is executed.
+    //     }
+    // });
 });
 /*
  * active menu widget for Silex
